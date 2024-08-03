e24e439369dcfd82fef43e8bcd92bbb4cad4b424
deepakdroi <deepak.mahto706@gmail.com>
2024-08-03 13:19:02 +0530

    pages were added for the link in navbar

---
+++ src/app/members/page.tsx
@@ -0,0 +1,10 @@
+import { Button, Link } from '@nextui-org/react'
+import React from 'react'
+
+export default function Menberspage() {
+  return (<div><h1>This is members page</h1>
+  <Button as={Link} href='/' color='primary'>Home</Button>
+  </div>
+    
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@nextui-org/react";
+import { Button, Link } from "@nextui-org/react";
 import { FaRegSmile } from "react-icons/fa";
 
 export default function Home() {
   return <div>
     <h1 className="text-3xl">Hello World !!!!!</h1>
-    <Button color="primary" variant="bordered" startContent={<FaRegSmile/>}>Click here.</Button>
+    <Button as={Link} href="/members" color="primary" variant="bordered" startContent={<FaRegSmile/>}>Click here.</Button>
   </div>;
   
 }
