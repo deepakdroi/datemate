23846f50eee05349cf15289ddc81f9ddb5b5bf16
deepakdroi <deepak.mahto706@gmail.com>
2025-02-16 23:36:25 +0530

    feat: Add user registration endpoint
    - Implemented API endpoint to handle user registration
    - Validates user input for required fields (username, email, password)
    - Hashes the user password before storing in the database
    - Returns a success message and status code upon successful registration
    - Includes error handling for duplicate email and other potential issues

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -2,32 +2,49 @@
 
 import { prisma } from "@/lib/prisma";
 import { registerSchema, RegisterSchema } from "@/lib/schemas/RegisterSchema";
+import { User } from "@prisma/client";
 import bcrypt from "bcryptjs";
 
-export async function registerUser(data: RegisterSchema) {
-	const validated = registerSchema.safeParse(data);
+export async function registerUser(
+  data: RegisterSchema
+): Promise<ActionResult<User>> {
+  try {
+    const validated = registerSchema.safeParse(data);
 
-	if (!validated.success) {
-		return { error: validated.error.errors };
-	}
+    if (!validated.success) {
+      return { status: "error", error: validated.error.errors };
+    }
 
-	const { name, age, gender, email, password } = validated.data;
+    const { name, age, gender, email, password } = validated.data;
 
-	const hashedPassword = await bcrypt.hash(password, 10);
-	console.log(data);
-	const userExists = await prisma.user.findUnique({
-		where: { email },
-	});
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log(data);
+    const userExists = await prisma.user.findUnique({
+      where: { email },
+    });
 
-	if (userExists) return "User already exists! Please try loging in.";
+    if (userExists)
+      return {
+        status: "error",
+        error: "User already exists! Please try logging in.",
+      };
 
-	return prisma.user.create({
-		data: {
-			name,
-			age,
-			gender,
-			email,
-			hashedPassword: hashedPassword,
-		},
-	});
+    const user = await prisma.user.create({
+      data: {
+        name,
+        age,
+        gender,
+        email,
+        hashedPassword: hashedPassword,
+      },
+    });
+
+    if (!user) {
+      return { status: "error", error: "Failed to create user." };
+    }
+    return { status: "success", data: user };
+  } catch (error) {
+    console.log(error);
+    return { status: "error", error: "An error occurred while creating user" };
+  }
 }
